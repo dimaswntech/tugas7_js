3ac35082dd33aecf95586a0d433c2343134f7ebe
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:05:10 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,21 @@
+// 1. Pastikan Teman Teman Sudah Menginstall Node JS
+
+// 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas7.js
+
+// 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+
+// Buatlah sebuah variabel array dengan maksimal data 10 nilai yang berisikan tinggi badan;
+// Tampilkan array tersebut dengan menggunakan perulangan biasa dan perulangan for off.
+// Teman-teman analisis perbedaan hasilnya,
+// Hint : -
+
+// 4. Happy Codding :)
+
+var tinggi = [189,188,187,186,185,184,183,181];
+for(var i = 0;i<tinggi.length;i++){
+    console.log(tinggi[i]);
+}
+console.log("");
+for(let x of tinggi){
+    console.log(x);
+}
